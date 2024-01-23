3d71dc633266a55b10665144a7d91ffba52c5a3c
SerenaMalusa <sere.malu@gmail.com>
2024-01-23 15:11:29 +0100

    fix: better variable name (for me)

--- js/functions.js
+++ js/functions.js
@@ -8,16 +8,16 @@ function printDeatils(container) {
         let person = team[member];
 
         // creo un ciclo che vada dal primo all'ultimo argomento del oggetto
-        for (let detail in person) {
+        for (let detailKey in person) {
 
             // recupero il valore della dell'argomento in una variabile
-            let detailValue = person[`${detail}`];
+            let detailValue = person[`${detailKey}`];
             
             // creo un nuovo elemento html .card
             let card = document.createElement('div');
             // l'HTML di card dovrà contenere gli elementi presi dall'oggetto
             card.innerHTML = `
-            <p>${detail}: ${detailValue}</p>
+            <p>${detailKey}: ${detailValue}</p>
             `
             // 
             container.appendChild(card);
